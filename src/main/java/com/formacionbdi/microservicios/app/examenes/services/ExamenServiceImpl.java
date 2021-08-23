0d3f75a0cf52c035d0c19f85cbc16656f549f7fb
Ruben-developer <rcontreras.dev@protonmail.com>
2021-08-22 20:26:31 -0400

    Se añaden componentes de service y controller para las asignaturas

--- src/main/java/com/formacionbdi/microservicios/app/examenes/services/ExamenServiceImpl.java
+++ src/main/java/com/formacionbdi/microservicios/app/examenes/services/ExamenServiceImpl.java
@@ -2,20 +2,34 @@ package com.formacionbdi.microservicios.app.examenes.services;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.formacionbdi.microservicios.app.examenes.models.repository.AsignaturarRepository;
 import com.formacionbdi.microservicios.app.examenes.models.repository.ExamenRepository;
+import com.formacionbdi.microservicios.commons.examenes.models.entity.Asignatura;
 import com.formacionbdi.microservicios.commons.examenes.models.entity.Examen;
 import com.formaciondbi.microservicios.commons.services.CommonServiceImpl;
 
 @Service
 public class ExamenServiceImpl extends CommonServiceImpl<Examen, ExamenRepository> implements ExamenService {
 
+	@Autowired
+	private AsignaturarRepository asignaturarRepository;
+	
 	@Override
 	@Transactional(readOnly = true)
 	public List<Examen> findByNombre(String term){
 		return repository.findByNombre(term);
 	}
+	
+
+	@Override
+	@Transactional(readOnly = true)
+	public Iterable<Asignatura> findAllAsignaturas(){
+		return asignaturarRepository.findAll();
+	}
+	
 	
 }
